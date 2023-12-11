853595e54514a82926008307e70880c112fc451f
MSBrett <MSBrett@users.no-reply.github.com>
2023-12-11 14:27:34 -0800

    Add storage

--- 06-AKS-supporting/main.tf
+++ 06-AKS-supporting/main.tf
@@ -4,6 +4,17 @@
 
 # Data From Existing Infrastructure
 
+data "terraform_remote_state" "aad" {
+  backend = "azurerm"
+
+  config = {
+    storage_account_name = var.state_sa_name
+    container_name       = var.container_name
+    key                  = "aad"
+    access_key           = var.access_key
+  }
+}
+
 data "terraform_remote_state" "existing-lz" {
   backend = "azurerm"
 
@@ -11,6 +22,17 @@ data "terraform_remote_state" "existing-lz" {
     storage_account_name = var.state_sa_name
     container_name       = var.container_name
     key                  = "lz-net"
+    access_key           = var.access_key
+  }
+}
+
+data "terraform_remote_state" "existing-hub" {
+  backend = "azurerm"
+
+  config = {
+    storage_account_name = var.state_sa_name
+    container_name       = var.container_name
+    key                  = "hub-net"
     access_key           = var.access_key
   }
 }
--- 06-AKS-supporting/storage.tf
+++ 06-AKS-supporting/storage.tf
@@ -1,43 +1,64 @@
-## Create a Resource Group for Storage
-resource "azurerm_resource_group" "rg_storage" {
-  location = var.deploy_location
-  name     = var.rg_stor
-}
 
-# generate a random string (consisting of four characters)
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "random" {
-  length  = 4
+  length  = 8
   upper   = false
   special = false
 }
 
-## Azure Storage Accounts requires a globally unique names
-## https://docs.microsoft.com/en-us/azure/storage/common/storage-account-overview
-## Create a File Storage Account 
-resource "azurerm_storage_account" "storage" {
+resource "azurerm_storage_account" "storage_account" {
   name                     = "stor${random_string.random.id}"
-  resource_group_name      = azurerm_resource_group.rg_storage.name
-  location                 = azurerm_resource_group.rg_storage.location
+  resource_group_name      = data.terraform_remote_state.existing-lz.outputs.lz_rg_name
+  location                 = data.terraform_remote_state.existing-lz.outputs.lz_rg_location
   account_tier             = "Premium"
   account_replication_type = "LRS"
   account_kind             = "FileStorage"
 }
 
-resource "azurerm_storage_share" "FSShare" {
-  name                 = "fslogix"
-  storage_account_name = azurerm_storage_account.storage.name
-  depends_on           = [azurerm_storage_account.storage]
+resource "azurerm_storage_share" "share1" {
+  name                 = "share1"
+  storage_account_name = azurerm_storage_account.storage_account.name
+  quota                = 1024
+  enabled_protocol     = "NFS"
 }
 
-## Azure built-in roles
-## https://docs.microsoft.com/en-us/azure/role-based-access-control/built-in-roles
-data "azurerm_role_definition" "storage_role" {
-  name = "Storage File Data SMB Share Contributor"
+# Deploy DNS Private Zone for storage
+
+resource "azurerm_private_dns_zone" "storage_account" {
+  name                = "privatelink.file.core.windows.net"
+  resource_group_name = data.terraform_remote_state.existing-lz.outputs.lz_rg_name
 }
 
-resource "azurerm_role_assignment" "af_role" {
-  scope              = azurerm_storage_account.storage.id
-  role_definition_id = data.azurerm_role_definition.storage_role.id
-  principal_id       = azuread_group.aad_group.id
+resource "azurerm_private_dns_zone_virtual_network_link" "lz_storage_account" {
+  name                  = "vnet_ai_link_to_storage"
+  resource_group_name   = data.terraform_remote_state.existing-lz.outputs.lz_rg_name
+  private_dns_zone_name = azurerm_private_dns_zone.storage_account.name
+  virtual_network_id    = data.terraform_remote_state.existing-lz.outputs.lz_vnet_id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "hub_storage_account" {
+  name                  = "vnet_hub_link_to_storage"
+  resource_group_name   = data.terraform_remote_state.existing-lz.outputs.lz_rg_name
+  private_dns_zone_name = azurerm_private_dns_zone.storage_account.name
+  virtual_network_id    = data.terraform_remote_state.existing-hub.outputs.hub_vnet_id
+}
+
+# storage Private Endpoint
+
+resource "azurerm_private_endpoint" "storage_endpoint" {
+  name                = "${azurerm_storage_account.storage_account.name}-ep"
+  location            = data.terraform_remote_state.existing-lz.outputs.lz_rg_location
+  resource_group_name = data.terraform_remote_state.existing-lz.outputs.lz_rg_name
+  subnet_id           = data.terraform_remote_state.existing-hub.outputs.hub_vnet_id
+
+  private_service_connection {
+    name                           = "${azurerm_storage_account.storage_account.name}-privateserviceconnection"
+    private_connection_resource_id = azurerm_storage_account.storage_account.id
+    subresource_names              = ["file"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name = "storage-endpoint-zone"
+    private_dns_zone_ids = [azurerm_private_dns_zone.storage_account.id]
+  }
 }
